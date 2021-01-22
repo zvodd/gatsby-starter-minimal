7c71395744764e4f3cbd6ade7a6339d60602e265
zvodd <zv.odd.101@gmail.com>
2021-01-22 17:45:29 +0000

    changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,29 @@
-module.exports = {
-    plugins: [
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
-    ]
+module.exports = {
+  siteMetadata: {
+    title: `Example Gatsby Cart`,
+    description: `Carts Carts Carts`,
+    author: `zvodd`,
+    siteUrl: `https://example.com`,
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-sitemap`,
+    "gatsby-plugin-robots-txt",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/src/assets`,
+      },
+    },
+  ],
 }
